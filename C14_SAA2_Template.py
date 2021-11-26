62936830a0f2bac9456b09a61fcbf170ae5d7927
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-26 12:36:07 +0530

    Add files via upload

---
+++ C14_SAA2_Template.py
@@ -0,0 +1,23 @@
+# Define the class Family
+
+    # Define the __init__ method with object properties- firstname, lastname, age, relation
+
+
+
+
+
+    
+    # Define a method full_name to print firstname concatenated with last name
+
+
+
+# Create the objects father, mother, sister and brother
+
+
+
+
+
+# Print the full names of each family member
+
+
+
